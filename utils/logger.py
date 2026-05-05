9b71f24847ee8978669c86f2e661f017a0a8c2bb
Jose David Ricardo <josedavidricardo28@gmail.com>
2026-05-04 20:50:58 -0500

    Delete utils directory

--- utils/logger.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-def configurar_logger():
-    logging.basicConfig(
-        filename="logs.txt",
-        level=logging.INFO,
-        format="%(asctime)s - %(levelname)s - %(message)s"
-    )
